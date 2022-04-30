740336c317440f4280bf5af84af7e9a249a7cd67
Sophia <sophiansl2018@yahoo.com>
2022-04-30 23:58:24 +1000

    05_object/hash table added

---
+++ 05_object.js
@@ -0,0 +1,50 @@
+// coding out object from scratch
+
+class HashTable {
+    constructor(size){
+        this.data = new Array(size);
+    }
+
+    _hash(key){
+        let hash = 0;
+        for (let i = 0; i < key.length; i++){
+            hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+        }
+        return hash;
+    }
+
+    set(key,value){
+        const address = this._hash(key);
+
+        if(!this.data[address]){
+            this.data[address] = [];
+        }
+        this.data[address].push([key,value]);
+        return this.data;
+    }
+
+    get(key){
+        const address = this._hash(key);
+        const bucket = this.data[address];
+
+        if(bucket){
+            for(let i = 0; i < bucket.length; i++){
+                if(bucket[i][0] === key){
+                    return bucket[i][1];
+                }
+            }
+        }
+        return undefined
+    }
+
+}
+
+const hash = new HashTable(20);
+hash.set('test',1);
+hash.set('some other key','some other value');
+console.log(hash.get('test'));
+console.log(hash.get('some other key'));
+console.log(hash.get('test2'));
+
+
+
