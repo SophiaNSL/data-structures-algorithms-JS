faabcfbbdf2105fd09d322147ab14a9990b5d468
Sophia <sophiansl2018@yahoo.com>
2022-05-18 23:08:23 +1000

    Adjacency List Added

--- 07_graph.js
+++ 07_graph.js
@@ -83,3 +83,63 @@ const isConnected2 = (node1, node2) => {
 
 console.log(findAdjacencies2('D'));
 console.log(isConnected2('C','D'));
+
+// Section Three: Adjacency List
+
+class Node {
+    constructor(value){
+        this.value = value;
+        this.edgesList = [];
+    }
+
+    connect(node){
+        this.edgesList.push(node);
+        node.edgesList.push(this);
+    }
+
+    //find adjacencies
+    findAdjacencies3(){
+        return this.edgesList.map(edge => edge.value);
+    }
+
+    //is connected
+    isConnected3(node){
+        return this.edgesList.some( edge => edge.value.indexOf(node.value) > -1);
+    }
+
+}
+
+class Graph {
+    constructor(nodes){
+        this.nodes = [...nodes]
+    }
+
+    addToGraph(node){
+        this.nodes.push(node);
+    }
+}
+
+const nodeA = new Node('A');
+const nodeB = new Node('B');
+const nodeC = new Node('C');
+const nodeD = new Node('D');
+const nodeE = new Node('E');
+
+const graph = new Graph([nodeA, nodeB, nodeC, nodeD, nodeE]);
+
+nodeA.connect(nodeB);
+nodeA.connect(nodeD);
+nodeB.connect(nodeC);
+nodeC.connect(nodeD);
+nodeC.connect(nodeE);
+nodeD.connect(nodeE);
+
+// for(let node of graph.nodes){
+//     console.log(`Node ${node.value}`);
+//     for(let connectedNode of node.edgesList){
+//         console.log(`Node ${node.value} is connected to ${connectedNode.value}`);
+//     }
+// }
+
+console.log(nodeD.findAdjacencies3());
+console.log(nodeB.isConnected3(nodeD));
